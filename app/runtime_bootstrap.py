c5f896c7a6b40ce4aeb3b6c35ee3d5681d5b34ce
GGBOND121382 <143927551+GGBOND121382@users.noreply.github.com>
2026-07-16 18:17:35 +0800

    use G3-capable runtime adapter

--- app/runtime_bootstrap.py
+++ app/runtime_bootstrap.py
@@ -9,7 +9,7 @@ def install_runtime_extensions() -> None:
     from . import llm as llm_module
     from . import workflows as workflows_module
     from .runtime_context import LiveContextBuilder
-    from .runtime_executor import RuntimePromptExecutor
+    from .g3_runtime_executor import G3RuntimePromptExecutor
     from .runtime_export import RecoverableDocxExporter
     from .full_integration_quality import FullProposalQualityGuard
     from .runtime_gateway import AuditedModelGateway
@@ -17,7 +17,7 @@ def install_runtime_extensions() -> None:
 
     context_module.ContextBuilder = LiveContextBuilder
     executor_module.ProposalQualityGuard = FullProposalQualityGuard
-    executor_module.PromptExecutor = RuntimePromptExecutor
+    executor_module.PromptExecutor = G3RuntimePromptExecutor
     llm_module.ModelGateway = AuditedModelGateway
     workflows_module.WorkflowEngine = RecoverableWorkflowEngine
     exporter_module.DocxExporter = RecoverableDocxExporter
